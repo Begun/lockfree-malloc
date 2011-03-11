3643f05fa0c2bfb62a1d6d6fdca594b3332c43d5
tkatchev <itkachev@begun.ru>
2011-03-11 15:44:42 +0300

    Licensing headers in sources.

--- lite-hooks.h
+++ lite-hooks.h
@@ -1,3 +1,17 @@
+/* Copyright 2011 ZAO "Begun".
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __LOCKFREE_LITE_HOOKS_H
 #define __LOCKFREE_LITE_HOOKS_H 1
 
